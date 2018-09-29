c8d86baedbb1c775ad8a43f4e82d936db298a223
Omar Hanafi <omarhanafi789@gmail.com>
2018-09-29 17:25:46 +0100

    Adding Mobile Inputs

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    [SerializeField] float thrustSpeed = 50f;
+    [SerializeField] float turnSpeed = 2000f;
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 position = transform.position;
+        position.Set(position.x, position.y, position.z + thrustSpeed * Time.deltaTime);
+        transform.position = position;
+
+        if (Input.GetMouseButton(0))
+        {
+            if(Input.mousePosition.x < 120)
+            {
+                rigidBody.AddForce(-turnSpeed * Time.deltaTime,0, 0);
+            } else if (Input.mousePosition.x > 230)
+            {
+                rigidBody.AddForce(turnSpeed * Time.deltaTime,0, 0);
+            }
+        }
+	}
+
+    void Movement ()
+    {
+
+    }
+}

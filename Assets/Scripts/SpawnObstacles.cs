35c25875a5f99ac582a4b0b76dac632ea3762847
DESKTOP-CL0UM13\Adib_ <adib_houssam@hotmail.com>
2018-10-03 17:53:54 +0100

    po

---
+++ Assets/Scripts/SpawnObstacles.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObstacles : MonoBehaviour {
+
+    public GameObject Obstacle;
+
+
+    // Use this for initialization
+    void Start () {
+        for (float z = 10; z < 1600; z += 15)
+        {
+            //5 per line
+            for (float x = 0; x < 5; x++)
+            {
+                Instantiate(Obstacle, new Vector3(Random.Range(-23.5f, 23.5f),1.5f, 400f + z), Quaternion.identity);
+
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

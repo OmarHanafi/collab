e81a63d7ada2b9f834c3073d4f3f9f3e150b8e8f
Omar Hanafi <omarhanafi789@gmail.com>
2018-10-04 17:11:17 +0100

    Removing Destroy Script

--- Assets/Scripts/SpawnObstacles.cs
+++ Assets/Scripts/SpawnObstacles.cs
@@ -6,11 +6,7 @@ public class SpawnObstacles : MonoBehaviour {
 
     public GameObject Obstacle;
     [SerializeField] GameObject player;
-<<<<<<< HEAD
     [SerializeField] float obstacleOffset = 50f;
-=======
-    [SerializeField] float obstacleOffset = 10f;
->>>>>>> dd62fd68e874dc87101680b0f3842bfd0e397626
 
     // Use this for initialization
     void Start () {
@@ -21,11 +17,8 @@ public class SpawnObstacles : MonoBehaviour {
         float z = player.transform.position.z + obstacleOffset;
         for (float x = 0; x < Random.Range(3, 5); x++)
         {
-<<<<<<< HEAD
-            Instantiate(Obstacle, new Vector3(Random.Range(-30f, 30f), Random.Range(1.5f, 5f), z), Quaternion.identity);
-=======
-            Instantiate(Obstacle, new Vector3(Random.Range(-24f, 24f), Random.Range(1.5f, 15f), z), Quaternion.identity);
->>>>>>> dd62fd68e874dc87101680b0f3842bfd0e397626
+            GameObject obs = Instantiate(Obstacle, new Vector3(Random.Range(-24f, 24f), Random.Range(1.5f, 15f), z), Quaternion.identity);
+            Destroy(obs, 3f);
         }
     }
 
